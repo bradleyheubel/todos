5e2fd6e93003a93eaa11c052613abab4ef940b0a
Brad <bradmta@gmail.com>
2020-05-20 21:23:42 +1000

    First Commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const electron = require('electron')
+
+const { app, BrowserWindow, Menu, ipcMain } = electron
+
+let mainWindow
+let addWindow
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({webPreferences:{nodeIntegration: true}})
+    mainWindow.loadURL(`file://${__dirname}/main.html`)
+    mainWindow.on('closed', () => app.quit())
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+})
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        webPreferences:{nodeIntegration: true},
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    })
+    addWindow.loadURL(`file://${__dirname}/add.html`)
+    
+    //Garbage collection
+    addWindow.on('closed', () => addWindow = null)
+}
+
+function clearTodos() {
+    mainWindow.webContents.send('todo:clear')
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:add', todo)
+    addWindow.close()
+})
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Todo',
+                click() { createAddWindow() }
+            },
+            {
+                label: 'Clear Todos',
+                click() { clearTodos() }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit()
+                }
+            }
+        ]
+    }
+]
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({})
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'DEBUG',
+        submenu: [
+            { role: 'reload' },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools()
+                }
+            }
+        ]
+    })
+}
